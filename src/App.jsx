c8f6e35ae271021d810f1c1f044cb80c5a752f06
sqwyer <570000826@nlrsd.org>
2024-04-12 14:17:54 -0500

    Change one on one to 'personalized'

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         <h1 className="text-2xl md:text-5xl font-bold">
           Pulliam-Bivens Tutoring
         </h1>
-        <p className="text-slate-600">One on one tutoring with a perfect scoring PSAT and ACT scorer. Contact us for rates and availability.</p>
+        <p className="text-slate-600">Personalized tutoring with a perfect scoring PSAT and ACT scorer. Contact us for rates and availability.</p>
         <a href="mailto:jbpulliam06@icloud.com" className="flex flex-row gap-2 items-center cursor-pointer text-blue-600">
           <img src={Mail} alt="Email: " className="h-4" /> <span>jbpulliam06@icloud.com</span>
         </a>
