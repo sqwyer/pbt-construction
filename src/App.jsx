952faa87bfdad3b28770d637397060b80fc58d99
sqwyer <570000826@nlrsd.org>
2024-04-12 19:35:47 -0500

    Fix visibility on mobile + reword tagline

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,19 @@ function App() {
         <h1 className="text-2xl md:text-5xl font-bold">
           Pulliam-Bivens Tutoring
         </h1>
-        <p className="text-slate-600">Personalized tutoring with a perfect scoring PSAT and ACT scorer. Contact us for rates and availability.</p>
+        <p className="text-slate-600">Personalized tutoring with a <span className="italic">perfect</span> scoring PSAT and ACT student. Contact us for rates and availability.</p>
         <div className="flex flex-col items-center sm:flex-row gap-2">
           <a href="mailto:jbpulliam06@icloud.com" className="flex flex-row gap-2 items-center cursor-pointer text-blue-600">
             <img src={Mail} alt="Email: " className="h-4" /> <span>jbpulliam06@icloud.com</span>
           </a>
-          <span className="text-gray-600 hidden sm:visible">•</span>
+          <span className="text-gray-600 hidden sm:block">•</span>
           <a href="mailto:sawyerbivens06@icloud.com" className="flex flex-row gap-2 items-center cursor-pointer text-blue-600">
             <img src={Mail} alt="Email: " className="h-4" /> <span>sawyerbivens06@gmail.com</span>
           </a>
         </div>
       </div>
 
-      <div className="p-4 bg-gray-50 rounded-md max-w-96 text-left flex flex-col gap-2">
+      <div className="p-4 bg-gray-50 italic rounded-md max-w-96 text-left flex flex-col gap-2">
         <p className="text-xs sm:text-sm">
           &quot;Jonah Pulliam and Sawyer Bivens have been some of my most dedicated and hard-working students. I am sure that they can help improve your ACT score.&quot;
         </p>
